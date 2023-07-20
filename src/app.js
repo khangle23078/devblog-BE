4aae04254b0c9a56f3a877421af7c9cb4b3783d0
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-07-20 16:25:32 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+const app = express()
+app.use(helmet())
+app.use(morgan('combined'))
+
+export default app;
