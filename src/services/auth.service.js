da1eb5859ab9e1b90e5aa32de7d3da58cde12849
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-07-25 16:18:30 +0700

    check email exits in register user

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,6 @@
 import User from './../models/user.model.js'
 
-export const registerUser = (data) => {
-  return User.create(data)
+export const emailUserExits = (email) => {
+  return User.findOne({ email: email }).exec()
 }
+
