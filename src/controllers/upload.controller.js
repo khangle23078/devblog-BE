a5853c4b370c294d1251bdd42221dd809a59af52
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-08-11 06:28:44 +0700

    feat: delete file

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,3 +1,5 @@
+import { v2 as cloudinary } from 'cloudinary';
+
 export const uploadFile = (req, res) => {
   try {
     const file = req.file;
@@ -9,6 +11,24 @@ export const uploadFile = (req, res) => {
         public_id: file.filename
       },
       message: "Upload file success"
+    })
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      error: true,
+      message: error.message
+    })
+  }
+}
+
+export const deleteFile = async (req, res) => {
+  try {
+    const { public_id } = req.body;
+    await cloudinary.uploader.destroy(public_id);
+    return res.status(200).json({
+      status: 200,
+      error: false,
+      message: 'Delete file success'
     })
   } catch (error) {
     return res.status(500).json({
--- src/routes/upload.route.js
+++ src/routes/upload.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { checkAuth } from './../middlewares/auth.middleware.js'
-import { uploadFile } from "../controllers/upload.controller";
+import { deleteFile, uploadFile } from "../controllers/upload.controller";
 import { upload } from './../config/storage.config.js'
 
 const route = Router()
 
 route.post('/file/upload', checkAuth, upload.single('image'), uploadFile,)
+route.delete('/file/destroy', checkAuth, deleteFile)
 
 export default route;
