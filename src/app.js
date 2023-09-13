25ac173eba03c818af51096fee13180993b029f0
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-09-13 18:58:56 +0700

    install cors

--- src/app.js
+++ src/app.js
@@ -6,9 +6,11 @@ import authRoute from './routes/auth.route.js'
 import categoryRoute from './routes/category.route.js'
 import postRoute from './routes/post.route.js'
 import uploadRoute from './routes/upload.route.js'
+import cors from 'cors';
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use(helmet())
 app.use(morgan('common'))
 app.use('/api', authRoute)
