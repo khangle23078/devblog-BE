118d202898c5e904528466d1e3237d467845a958
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-11-21 17:15:51 +0700

    set limit payload

--- src/app.js
+++ src/app.js
@@ -1,22 +1,29 @@
-import express from 'express'
-import helmet from 'helmet'
-import morgan from 'morgan'
-import { connectDb } from './database/connectDb.js'
-import authRoute from './routes/auth.route.js'
-import categoryRoute from './routes/category.route.js'
-import postRoute from './routes/post.route.js'
-import uploadRoute from './routes/upload.route.js'
-import cors from 'cors';
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import { connectDb } from "./database/connectDb.js";
+import authRoute from "./routes/auth.route.js";
+import categoryRoute from "./routes/category.route.js";
+import postRoute from "./routes/post.route.js";
+import uploadRoute from "./routes/upload.route.js";
+import cors from "cors";
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(helmet())
-app.use(morgan('common'))
-app.use('/api', authRoute)
-app.use('/api', categoryRoute)
-app.use('/api', postRoute)
-app.use('/api', uploadRoute)
-connectDb()
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000
+  })
+);
+app.use(cors());
+app.use(helmet());
+app.use(morgan("common"));
+app.use("/api", authRoute);
+app.use("/api", categoryRoute);
+app.use("/api", postRoute);
+app.use("/api", uploadRoute);
+connectDb();
 
 export default app;
