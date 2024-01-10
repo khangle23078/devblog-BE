23289d1ea0f923581e67882e3343a4685e6eaa96
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2024-01-10 18:17:15 +0700

    remove checkAuth in get post route

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -4,8 +4,8 @@ import { createPost, deletePost, editPost, getPost, getPosts } from "../controll
 
 const route = Router()
 
-route.get('/posts/', checkAuth, getPosts)
-route.get('/posts/:id', checkAuth, getPost)
+route.get('/posts/', getPosts)
+route.get('/posts/:id', getPost)
 route.post('/posts', checkAuth, createPost)
 route.put('/posts/:id', checkAuth, editPost)
 route.delete('/posts/:id', checkAuth, deletePost)
